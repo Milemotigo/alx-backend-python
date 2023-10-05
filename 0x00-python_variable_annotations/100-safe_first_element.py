2267ddc294d1e021b3d69f46f16e944d5596b0e6
milemotigo <mtigo@gmail.com>
2023-10-05 21:44:04 +0100

    task 8

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+from typing import Sequence, Union, Any
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    if lst:
+        return lst[0]
+    else:
+        return None
+    
+print(safe_first_element.__annotations__)
