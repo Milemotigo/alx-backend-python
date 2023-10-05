c42b0ff9111add913e264cb280722dd6a5989083
milemotigo <mtigo@gmail.com>
2023-10-05 19:05:59 +0100

    new commit

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from typing import List
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    Calculate the sum of a list of floats.
+
+    Args:
+        input_list (List[float]): The list of floats to be summed.
+
+    Returns:
+        float: The sum of the input list.
+    """
+    return sum(input_list)
+
