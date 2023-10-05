f2bf7947848375590a5969505d53c8cb2f442ce6
milemotigo <mtigo@gmail.com>
2023-10-05 14:48:19 +0100

    first commit

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+def floor(n: float) -> int:
+    """
+    Return the floor of a float.
+
+    Args:
+        n (float): The input float.
+
+    Returns:
+        int: The floor of the input float.
+    """
+    return int(n)
