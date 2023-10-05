bfcb754e1b752e5f581979b6034b02606cb7918f
milemotigo <mtigo@gmail.com>
2023-10-05 18:59:27 +0100

    new commit

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -10,8 +10,3 @@ def to_str(n: float) -> str:
         str: The string of the input float.
     """
     return str(n)
-
-pi_str = to_str(3.14)
-print(pi_str == str(3.14))
-print(to_str.__annotations__)
-print("to_str(3.14) returns {} which is a {}".format(pi_str, type(pi_str)))
