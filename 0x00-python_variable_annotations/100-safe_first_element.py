b8772d012ed64f0d446de6faaed20908b58f2f37
milemotigo <mtigo@gmail.com>
2023-10-05 21:53:52 +0100

    task 100

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -2,6 +2,16 @@
 from typing import Sequence, Union, Any
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """
+    Safely retrieve the first element of a sequence.
+
+    Args:
+        lst (Sequence[Any]): A sequence (e.g., list, tuple) of any type.
+
+    Returns:
+        Union[Any, None]: The first element of the sequence if it is not empty.
+                          None if the sequence is empty.
+    """
     if lst:
         return lst[0]
     else:
