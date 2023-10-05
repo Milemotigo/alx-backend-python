7fa7dddba4a08a85778b2108179e42d99e5e9201
milemotigo <mtigo@gmail.com>
2023-10-05 15:25:45 +0100

    fix

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -4,12 +4,5 @@
 def concat(str1: str, str2: str) -> str:
     '''
     This function takes two arguments and returns their concatenation as a string.
-
-    Args:
-        arg1 (int): The first integer argument.
-        arg2 (str): The second string argument.
-
-    Returns:
-        str: The concatenated string.
     '''
     return str1 + str2
