79842068e2047d23745f3c75c93fcbc85e7c1897
milemotigo <mtigo@gmail.com>
2023-10-06 06:57:07 +0100

    Last commit

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 """
-11. Safely get a value from a dictionary using a specified key.
+ 101. More involved type annotations
 """
-from typing import TypeVar, Mapping, Any, Union
-
+from typing import TypeVar, Union, Any, Mapping
 
 T = TypeVar('T')
+V = Union[Any, T]
+Df = Union[T, None]
 
-def safely_get_value(dct: Mapping[str, Any], key: Any, default: Union[T, None] = None) -> Union[Any, T]:
-    """Safely get a value from a dictionary using a specified key."""
+
+def safely_get_value(dct: Mapping, key: Any, default: Df = None) -> V:
+    """Safely get a value from a dictionary using a specified key. """
     if key in dct:
         return dct[key]
     else:
