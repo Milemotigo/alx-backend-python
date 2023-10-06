aa669c2b4e08c3c2e9c39339c5b5e24e86e52562
milemotigo <mtigo@gmail.com>
2023-10-06 07:04:47 +0100

    Last commit

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Augment the following code with the correct duck-typed annotations:
 """
-from typing import Sequence, Union, Any, Optional
+from typing import Sequence, Union, Any
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
