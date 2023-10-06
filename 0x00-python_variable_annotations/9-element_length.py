2e3458437aad808a85a1296ae1729a4978ef7d2a
milemotigo <mtigo@gmail.com>
2023-10-06 07:06:55 +0100

    Last commit

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-'''Annotate the below function’s parameters and return values with the appropriate types'''
+'''Annotate the below function'''
 from typing import List, Iterable, Sequence, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
-    """ Calculate the length of each element in the input list and return a list of tuples. """
+    """Calculate the length of each element in the input list."""
     return [(i, len(i)) for i in lst]
 
