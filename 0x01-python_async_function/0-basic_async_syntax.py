2a8efe2091b5875aef2df0e7eb0de7bf11bc1046
milemotigo <mtigo@gmail.com>
2023-10-09 19:30:59 +0100

    first commit

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""0. The basics of async"""
+import asyncio
+import random
+
+async def wait_random(max_delay: int = 10) -> float:
+    """an asynchronous coroutine that takes in an integer argumen
+    """
+    i = random.uniform(0, max_delay)
+    await asyncio.sleep(i)
+    return i
