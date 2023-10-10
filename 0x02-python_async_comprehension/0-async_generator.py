652e82941bdb4f396fe2f986f7732c69943b3e5c
milemotigo <mtigo@gmail.com>
2023-10-10 17:35:09 +0100

    task 1

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,15 @@
+#/usr/bin/env python3
+"""a coroutine called async_generator that takes no arguments."""
+
+import asyncio
+from typing import Generator
+import random
+
+
+async def async_generator() -> Generator[int, None, None]:
+    """The coroutine will loop 10 times, each time asynchronously wait 1 second, then yield a random
+    """
+    for i in range(10):
+        x = random.uniform(0, i)
+        yield x
+    
