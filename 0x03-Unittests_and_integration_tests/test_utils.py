a16734c37e7f153260c1a1369b88ab02e3bffde4
milemotigo <mtigo@gmail.com>
2023-10-29 20:53:04 +0100

    first

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -7,18 +7,27 @@ import unittest
 from utils import access_nested_map
 from parameterized import parameterized
 
-class TestAccessNestedMap(unittest.TestCase):
 
-   @parameterized.expand([
-    ({"a": 1}, ("a",), 1),
-    ({"a": {"b": 2}}, ("a",), {"b": 2}),
-    ({"a": {"b": 2}}, ("a", "b"), 2),
-    ])
-   
-   def test_access_nested_map(self, nested_map, path, expected_result):
+class TestAccessNestedMap(unittest.TestCase):
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+        ])
+    def test_access_nested_map(self, nested_map, path, expected_result):
         """Testing the access_nested_map"""
         result = access_nested_map(nested_map, path)
         self.assertEqual(result, expected_result)
 
-if __name__=="__main__":
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b")),
+        ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """manager to test that a KeyError"""
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
+
+
+if __name__ == "__main__":
     unittest.main()
