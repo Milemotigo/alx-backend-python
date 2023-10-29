60c720e159d4b8bd857f915aa02ee6d1c83639c1
milemotigo <mtigo@gmail.com>
2023-10-29 22:03:22 +0100

    first

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -9,25 +9,25 @@ from utils import access_nested_map, get_json
 from parameterized import parameterized
 
 
-class TestAccessNestedMap(unittest.TestCase):
-    @parameterized.expand([
-        ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2),
-        ])
-    def test_access_nested_map(self, nested_map, path, expected_result):
-        """Testing the access_nested_map"""
-        result = access_nested_map(nested_map, path)
-        self.assertEqual(result, expected_result)
+# class TestAccessNestedMap(unittest.TestCase):
+#     @parameterized.expand([
+#         ({"a": 1}, ("a",), 1),
+#         ({"a": {"b": 2}}, ("a",), {"b": 2}),
+#         ({"a": {"b": 2}}, ("a", "b"), 2),
+#         ])
+#     def test_access_nested_map(self, nested_map, path, expected_result):
+#         """Testing the access_nested_map"""
+#         result = access_nested_map(nested_map, path)
+#         self.assertEqual(result, expected_result)
 
-    @parameterized.expand([
-        ({}, ("a",)),
-        ({"a": 1}, ("a", "b")),
-        ])
-    def test_access_nested_map_exception(self, nested_map, path):
-        """manager to test that a KeyError"""
-        with self.assertRaises(KeyError):
-            access_nested_map(nested_map, path)
+#     @parameterized.expand([
+#         ({}, ("a",)),
+#         ({"a": 1}, ("a", "b")),
+#         ])
+#     def test_access_nested_map_exception(self, nested_map, path):
+#         """manager to test that a KeyError"""
+#         with self.assertRaises(KeyError):
+#             access_nested_map(nested_map, path)
 
 
 @parameterized.expand([
